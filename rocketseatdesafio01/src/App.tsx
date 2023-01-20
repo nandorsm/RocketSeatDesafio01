f427d8a782932e69c8e62d858b5a24eee85916d8
nandorsm <fernandocaetano933@gmail.com>
2023-01-20 00:35:39 -0300

    header

---
+++ rocketseatdesafio01/src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+import { Header } from "./components/Header";
+
+import './App.css'
+
+function App() {
+
+  return (
+    <Header/>
+  );
+}
+
+export default App
